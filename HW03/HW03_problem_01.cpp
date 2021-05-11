b265a329f4e663d8356157d1532cb057d6095dde
Daepoid <daepoid@gmail.com>
2021-05-11 23:48:12 +0900

    Update files

--- HW03/HW03_problem_01.cpp
+++ HW03/HW03_problem_01.cpp
@@ -1,10 +1,13 @@
 #include <algorithm>
+#include <cmath>
 #include <cstdio>
 #include <fstream>
 #include <iostream>
 #include <windows.h>
 #include <wingdi.h>
 #define MAX 512
+#define SNR 8.0
+#define THRESHOLD 150
 using namespace std;
 
 typedef struct headers {
@@ -56,62 +59,170 @@ void reverse_raw_data(BYTE *image) {
   }
 }
 
-void Add_Gaussian_Noise(BYTE *image) {
-  // 가우시안 노이즈를 생성하여
+double Calculate_Variance(BYTE *image) {
+  double avg_1, avg_2;
+  for (int i = 0; i < MAX; i++) {
+    for (int j = 0; j < MAX; j++) {
+      int temp = image[i * MAX + j];
+      avg_1 += temp;
+      avg_2 += temp * temp;
+    }
+  }
+
+  avg_1 /= (MAX * MAX);
+  avg_1 = pow(avg_1, 2);
+  avg_2 /= (MAX * MAX);
+
+  return avg_2 - avg_1;
 }
 
-void Edge_Detection_3(BYTE *image, int mask[][3], string output_name) {
+int Gaussian(double sigma) {
+  static int ready = 0;
+  static float gstore;
+  float v1, v2, r, fac, gaus;
+  int r1, r2;
+  if (ready == 0) {
+    do {
+      r1 = rand();
+      r2 = rand();
+      v1 = 2. * ((float)r1 / (float)RAND_MAX - 0.5);
+      v2 = 2. * ((float)r2 / (float)RAND_MAX - 0.5);
+      r = v1 * v1 + v2 * v2;
+    } while (r > 1.0);
+    fac = (float)sqrt((double)(-2 * log(r) / r));
+    gstore = v1 * fac;
+    gaus = v2 * fac;
+    ready = 1;
+  } else {
+    ready = 0;
+    gaus = gstore;
+  }
+  return gaus * sigma;
+}
+
+void Add_Gaussian_Noise(BYTE *image, BYTE *noise, double sigma) {
+  long long int a = sizeof(*noise);
+  for (int i = 0; i < MAX; i++) {
+    for (int j = 0; j < MAX; j++) {
+      int s = image[i * MAX + j] + Gaussian(sigma);
+      if (s > 255) {
+        s = 255;
+      }
+      if (s < 0) {
+        s = 0;
+      }
+      noise[i * MAX + j] = s;
+    }
+  }
+}
+
+bool isvalid(int newy, int newx) {
+  return 0 <= newy && newy < MAX && 0 <= newx && newx < MAX;
+}
+
+void masking(BYTE *noise, int y, int x, int mask[][3][3]) {
+  int avg_1 = 0, avg_2 = 0;
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      int newy = y + i - 1;
+      int newx = x + j - 1;
+
+      if (!isvalid(newy, newx)) {
+        // 임시로 zero padding을 수행
+        if (newy < 0) {
+          newy += 1;
+        } else if (newy >= MAX) {
+          newy -= 1;
+        }
+
+        if (newx < 0) {
+          newx += 1;
+        } else if (newx >= MAX) {
+          newx -= 1;
+        }
+        avg_1 += noise[newy * MAX + newx] * mask[0][i][j];
+        avg_2 += noise[newy * MAX + newx] * mask[1][i][j];
+      } else {
+        avg_1 += noise[newy * MAX + newx] * mask[0][i][j];
+        avg_2 += noise[newy * MAX + newx] * mask[1][i][j];
+      }
+    }
+  }
+  double val = sqrt(avg_1 * avg_1 + avg_2 * avg_2);
+
+  if (THRESHOLD <= val) {
+    noise[y * MAX + x] = (BYTE)255;
+  } else {
+    noise[y * MAX + x] = (BYTE)0;
+  }
+}
+
+void make_bmp(BYTE *output_image, string output_name) {
+  reverse_raw_data(output_image);
+
   BITMAPHEADERS bh;
   generate_headers(bh);
+  string PATH = "outputs/" + output_name + "_EdgeDetection.bmp";
+  FILE *output_file = fopen(PATH.c_str(), "wb");
+  fwrite(&bh.hFile, sizeof(BITMAPFILEHEADER), 1, output_file);
+  fwrite(&bh.hInfo, sizeof(BITMAPINFOHEADER), 1, output_file);
+  fwrite(bh.hRGB, sizeof(RGBQUAD), 256, output_file);
 
-  BYTE *noise = (BYTE *)malloc(sizeof(BYTE) * sizeof(image));
+  fwrite(output_image, sizeof(BYTE), MAX * MAX, output_file);
+  // fclose(output_file);
+  return;
+}
+
+void Edge_Detection_3(BYTE *image, int mask[][3][3], string output_name) {
+  BYTE *noise = (BYTE *)malloc(sizeof(BYTE) * MAX * MAX);
   memcpy(noise, image, sizeof(image));
 
-  // 입력받은 마스크를 아용하여 Edge Detection을 진행하고, 결과영상을 만든다.
+  BYTE *ori_image = (BYTE *)malloc(sizeof(BYTE) * MAX * MAX);
+  memcpy(ori_image, image, sizeof(image));
+
+  // 입력받은 마스크를 이용하여 Edge Detection을 진행하고, 결과영상을 만든다.
   // 추가로 계산된 오류값이 얼마인지도 콘솔로 출력해준다.
   // 콘솔로 출력하는 대신에 파일 이름으로 출력할 수 있다.
 
   // Original Image Edge Detection
+  for (int i = 0; i < MAX; i++) {
+    for (int j = 0; j < MAX; j++) {
+      masking(image, i, j, mask);
+    }
+  }
+  make_bmp(ori_image, "original_" + output_name);
 
   // Add Gaussian Noise
+  double variance = Calculate_Variance(image);
+  double stddev_noise = sqrt(variance / pow(10.0, ((double)SNR / 10)));
+  Add_Gaussian_Noise(image, noise, stddev_noise);
 
   // Noisy Image Edge Detection
   for (int i = 0; i < MAX; i++) {
     for (int j = 0; j < MAX; j++) {
-      
+      masking(noise, i, j, mask);
     }
   }
-
-  // Calculate Edge Detection Error Rate
-  reverse_raw_data(image);
-  string PATH = "outputs/" + output_name + "EdgeDetection.bmp";
-  FILE *output_file = fopen(PATH.c_str(), "wb");
-  fwrite(&bh.hFile, sizeof(BITMAPFILEHEADER), 1, output_file);
-  fwrite(&bh.hInfo, sizeof(BITMAPINFOHEADER), 1, output_file);
-  fwrite(bh.hRGB, sizeof(RGBQUAD), 256, output_file);
-  fwrite(image, sizeof(BYTE), MAX * MAX, output_file);
-  fclose(output_file);
-  // free(image);
+  make_bmp(noise, output_name);
 }
 
 void Stochastic(BYTE *image) {
-  BITMAPHEADERS bh;
-  generate_headers(bh);
-  BYTE *noise = (BYTE *)malloc(sizeof(BYTE) * sizeof(image));
+  double roberts[5][5] = {{0.267, 0.364, 0, -0.364, -0.267},
+                          {0.373, 0.562, 0, -0.562, -0.373},
+                          {0.463, 1.000, 0, -1.000, -0.463},
+                          {0.373, 0.562, 0, -0.562, -0.373},
+                          {0.267, 0.364, 0, -0.364, -0.267}};
+  BYTE *noise = (BYTE *)malloc(sizeof(BYTE) * MAX * MAX);
   memcpy(noise, image, sizeof(image));
 
-  reverse_raw_data(image);
-  string PATH = "outputs/Stochastic_EdgeDetection.bmp";
-  FILE *output_file = fopen(PATH.c_str(), "wb");
-  fwrite(&bh.hFile, sizeof(BITMAPFILEHEADER), 1, output_file);
-  fwrite(&bh.hInfo, sizeof(BITMAPINFOHEADER), 1, output_file);
-  fwrite(bh.hRGB, sizeof(RGBQUAD), 256, output_file);
-  fwrite(image, sizeof(BYTE), MAX * MAX, output_file);
-  fclose(output_file);
+  double variance = Calculate_Variance(image);
+  double stddev_noise = sqrt(variance / pow(10.0, ((double)SNR / 10)));
+  Add_Gaussian_Noise(image, noise, stddev_noise);
+  make_bmp(noise, "Stochastic");
 }
 
 int main() {
-
   BYTE *image;
   FILE *input_file;
   input_file = fopen("lena_raw_512x512.raw", "rb");
@@ -119,13 +230,18 @@ int main() {
   fread(image, sizeof(BYTE), MAX * MAX, input_file);
   fclose(input_file);
 
-  // Original Image Edge Detection
+  int roberts[2][3][3] = {{{0, 0, -1}, {0, 1, 0}, {0, 0, 0}},
+                          {{-1, 0, 0}, {0, 1, 0}, {0, 0, 0}}};
 
-  // Add Gaussian Noise
+  int prewitt[2][3][3] = {{{1, 0, -1}, {1, 0, -1}, {1, 0, -1}},
+                          {{-1, -1, -1}, {0, 0, 0}, {1, 1, 1}}};
 
-  // Noisy Image Edge Detection
+  int sobel[2][3][3] = {{{1, 0, -1}, {2, 0, -2}, {1, 0, -1}},
+                        {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}}};
 
-  // Calculate Edge Detection Error Rate
-
+  Edge_Detection_3(image, roberts, "Roberts");
+  Edge_Detection_3(image, sobel, "Sobel");
+  Edge_Detection_3(image, prewitt, "Prewitt");
+  Stochastic(image);
   return 0;
 }
